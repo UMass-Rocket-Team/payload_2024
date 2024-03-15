feb8082b358476d2a03b0a3dcbc2ba196a9d9882
Anton Voronov <53878857+ravenspired@users.noreply.github.com>
2024-03-14 21:42:09 -0400

    Create channel_monitor.lua

---
+++ lua-scripts/channel_monitor.lua
@@ -0,0 +1,15 @@
+
+
+local scripting_rc_1 = rc:find_channel_for_option(300)
+local scripting_rc_2 = rc:find_channel_for_option(301)
+
+
+function update ()
+
+chval = tostring(rc:get_pwm(11))
+gcs:send_text(0, chval)
+
+
+  return update, 250
+end
+return update, 1000
