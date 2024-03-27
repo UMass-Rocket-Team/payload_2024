231a0e5f9bd3a8e50336328d9a52069ddf9755da
Mitchell <belgianlion1234@icloud.com>
2024-03-26 22:15:07 -0500

    Payload-4: add decorator

--- lua-scripts/payload.lua
+++ lua-scripts/payload.lua
@@ -15,14 +15,23 @@ local MAX_NOSECONE_DEPLOYMENT_ALT = 500
 local MIN_NOSECONE_DEPLOYMENT_ALT = 400
 local MODES = {["STABILIZE"] = 0, ["ALT_HOLD"] = 2, ["RTL"] = 6}
 
+--Decorator to pass standardized data
+function standard_data_collection(f)
+  return function(self)
+    local altitude = alt()
+    gcs:send_text(0, "Altitude: " .. altitude)
+    return f(self, altitude)
+  end
+end
+
 function startup()
   -- Zero altitude (REMOVE IF THIS SCRIPT IS NOT STARTED ON LAUNCH)
   alt(0)
   vehicle:set_mode(MODES["STABILIZE"])
-  return idle, FREQUENCY
+  return standard_data_collection(idle(self, altitude)) FREQUENCY
 end
 
-function idle()
+function idle(altitude)
   local altitude = alt()
   gcs:send_text(0, "State: IDLE. Awaiting Arming")
   gcs:send_text(0, "Altitude: " .. altitude)
@@ -31,7 +40,7 @@ function idle()
   SRV_Channels:set_output_pwm(cone_servo_channel, LOCKED_CONE_SERVO_POS)
   -- Check for manual arming
   arming:is_armed() then
-    return check_deploy_cone, FREQUENCY
+    return standard_data_collection(check_deploy_cone(self, altitude)), FREQUENCY
   end
   -- This was here, but seems like it is unused?
   -- if rc:get_pwm(11) < DEPLOY_CHANNEL_THRESHOLD then
@@ -42,18 +51,17 @@ end
 -- Function: check_deploy_cone
 -- Description: Checks the altitude of the current position and deploys the payload cone if the altitude is within the specified range.
 -- Returns: Changes to state deploy_payload if the altitude is within the specified range, otherwise returns to check_deploy_cone
-function check_deploy_cone()
-  local altitude = alt()
+function check_deploy_cone(altitude)
   gcs:send_text(0, "State: CONE DEPLOYMENT. Checking for deployment altitude [min:" .. MIN_NOSECONE_DEPLOYMENT_ALT .. ", max:" .. MAX_NOSECONE_DEPLOYMENT_ALT .. "]")
   gcs:send_text(0, "Altitude: " .. altitude)
   if MAX_NOSECONE_DEPLOYMENT_ALT > altitude > MIN_NOSECONE_DEPLOYMENT_ALT then
     gcs:send_text(0, "Deploying nosecone...")
     SRV_Channels:set_output_pwm(payload_servo_channel, LOCKED_PAYLOAD_SERVO_POS) --redundant
     SRV_Channels:set_output_pwm(cone_servo_channel, UNLOCKED_CONE_SERVO_POS)
-    return deploy_payload FREQUENCY
+    return standard_data_collection(deploy_payload(self, altitude)), FREQUENCY
   end
 
-  return check_deploy_cone FREQUENCY
+  return standard_data_collection(check_deploy_cone(self, altidtude)), FREQUENCY
 end
 
 -- Function: deploy_payload
@@ -64,7 +72,7 @@ function deploy_payload()
   SRV_Channels:set_output_pwm(payload_servo_channel, UNLOCKED_PAYLOAD_SERVO_POS)
   SRV_Channels:set_output_pwm(cone_servo_channel, UNLOCKED_CONE_SERVO_POS) --redundant
   --NEED TO SEE HOW LONG WE NEED TO WAIT BEFORE VEHICLE LEAVES PAYLOAD, and if we need to check for any params
-  return await_altitude_hold FREQUENCY
+  return standard_data_collection(check_deploy_cone(self, alt)) FREQUENCY
 end
 
 function await_altitude_hold()
