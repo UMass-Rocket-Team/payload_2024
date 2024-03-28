c296889d35967706ce1c4762772ccf0daa412ead
Mitchell <belgianlion1234@icloud.com>
2024-03-27 22:36:27 -0500

    Payload-6: Add altitude test

---
+++ lua-scripts/testing-scripts/altitude_zeroing_test.lua
@@ -0,0 +1,14 @@
+local FEET_IN_METER 3.281
+
+function run_alt()
+    local dist = ahrs:get_relative_position_NED_home()
+    local altitudeMeters = -1*dist:z()
+    local altitudeFeet = altitudeMeters / FEET_IN_METER
+end
+
+function startup()
+    ahrs:set_home(ahrs:get_location())
+    return run_alt, 1000
+end
+
+return startup, 1000
