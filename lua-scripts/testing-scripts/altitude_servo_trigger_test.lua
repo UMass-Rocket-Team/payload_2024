9a9f77404c4dd070c0f09995e0aedac563a63c54
Mitchell <belgianlion1234@icloud.com>
2024-03-28 08:32:55 -0500

    Payload-6: Cleanup

--- lua-scripts/testing-scripts/altitude_servo_trigger_test.lua
+++ lua-scripts/testing-scripts/altitude_servo_trigger_test.lua
@@ -1,4 +1,4 @@
-local METERS_TO_FEET 3.281
+local FEET_IN_METER = 3.281
 --------Servo Positions---------
 --(likely need to be updated)---
 local LOCKED_PAYLOAD_SERVO_POS = 1000
@@ -22,19 +22,21 @@ function run_alt()
     local altitudeMeters = -1*dist:z()
     local altitudeFeet = altitudeMeters * METERS_TO_FEET
     gcs:send_text(0, "Altitude (ft): " .. altitudeFeet)
-    if MAX_NOSECONE_DEPLOYMENT_ALT > altitudeFeet
+    if MAX_NOSECONE_DEPLOYMENT_ALT > altitudeFeet then
         SRV_Channels:set_output_pwm(cone_servo_channel, UNLOCKED_CONE_SERVO_POS)
         gcs:send_text(0, "Nosecone Unlocked")
     else --else usage not great practice, but just for testing...
         SRV_Channels:set_output_pwm(cone_servo_channel, LOCKED_CONE_SERVO_POS)
     end
 
-    if MAX_PAYLOAD_DEPLOYMENT_ALT > altitudeFeet
+    if MAX_PAYLOAD_DEPLOYMENT_ALT > altitudeFeet then
         SRV_Channels:set_output_pwm(payload_servo_channel, UNLOCKED_PAYLOAD_SERVO_POS)
         gcs:send_text(0, "Payload Unlocked")
     else --else usage not great practice, but just for testing...
         SRV_Channels:set_output_pwm(payload_servo_channel, LOCKED_PAYLOAD_SERVO_POS)
     end
+
+    return run_alt, 250
 end
 
 function startup()
--- lua-scripts/testing-scripts/altitude_zeroing_test.lua
+++ lua-scripts/testing-scripts/altitude_zeroing_test.lua
@@ -1,9 +1,10 @@
-local FEET_IN_METER 3.281
+local FEET_IN_METER = 3.281
 
 function run_alt()
     local dist = ahrs:get_relative_position_NED_home()
     local altitudeMeters = -1*dist:z()
     local altitudeFeet = altitudeMeters / FEET_IN_METER
+    gcs:send_text(0, "Altitude (ft): " .. altitudeFeet)
 end
 
 function startup()
